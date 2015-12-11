a32840977c01899b39bf90961fec06236dd67bbd
poojagit <pooja.p.jain@gmail.com>
2015-12-11 12:04:28 +0530

    changed

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 var app = require("express")();
 var csv = require("csv");
 var obj = csv();
+
+
 
 function point(name,lat,lon){
     this.name = name;
@@ -17,6 +21,8 @@ obj.from.path('data/pdata.csv').to.array(function (data) {
     console.log(points);
 });
 
+
+
 app.get("/",function (req,res)
     {
         // now we use the templating capabilities of express and call our template to render the view, and pass a few parameters to it
@@ -24,5 +30,5 @@ app.get("/",function (req,res)
     });
 
  
-app.listen(9999);
+app.listen(server_port,server_ip_address);
 
